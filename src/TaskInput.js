f53aba2935c99ecd2832375781248fee77138ff7
Hoang Nguyen <hnguyen9@jacksonville.edu>
2024-11-22 22:38:01 -0500

    first commit

---
+++ src/TaskInput.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+const TaskInput = ({handleAddTask}) => {
+    const [task,setTask] = useState('');
+    const addTask = () => {
+        if (task.trim()){
+            handleAddTask(task);
+            setTask('');
+        }
+    };
+
+return(
+    <div>
+        <input
+        type="text"
+        value={task}
+        onChange={(e)=>setTask(e.target.value)}
+        placeholder="Enter a Task"
+        ></input>
+        <button onClick={addTask}>Add Task</button>
+    </div>
+)
+
+};
+export default TaskInput;
